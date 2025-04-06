8bd8d15aba73cad7a37334169c9be197cf1f60ac
gorkeygorkem <48758217+gorkeygorkem@users.noreply.github.com>
2025-04-06 23:24:35 +0300

    Update styling

--- screens/InputScreen.js
+++ screens/InputScreen.js
@@ -313,7 +313,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '600',
     color: '#fff',
-    marginBottom: 12,
+    marginVertical: 12,
   },
   labelfirst: {
     fontSize: 18,
